a9fa9a5f0b5e5d8a2f405bd32b46a590339e0c5b
Seppo Kilpelainen <seppo.kilpelainen@gmail.com>
2019-03-17 20:38:07 +0200

    changed the example to use the new interface

--- example/main.cpp
+++ example/main.cpp
@@ -9,28 +9,40 @@ int main(unsigned int argc, const char* argv[]) {
   af::array inputs = af::randu(af::dim4(1, 1, 1, 10000)) * af::Pi;
   af::array targets = af::sin(inputs);
 
-  auto provider = std::make_shared<BlobProvider>(BlobProvider(inputs.host<float>(),
-    targets.host<float>(), inputs.dims(), targets.dims()));
 
-  auto optimizer = std::make_shared<SGD>(1e-4f);
+  auto optimizer = std::make_shared<SGD>(1e-3f);
+  auto provider = TrainingBlob(inputs.host<float>(), targets.host<float>(),
+                               inputs.dims(), targets.dims());
 
-  Network nn(provider);
-  nn.add(optimizer);
-  auto fc1 = std::make_shared<FullyConnected>(100);
+  Network nn(provider, optimizer);
+
+  auto fc1 = std::make_shared<FullyConnected>(20);
   nn.add(fc1);
+
   nn.add(std::make_shared<Logistic>());
-  auto fc2 = std::make_shared<FullyConnected>(1);
+
+  auto fc2 = std::make_shared<FullyConnected>(20);
   nn.add(fc2);
+
   nn.add(std::make_shared<Logistic>());
+
+  auto fc3 = std::make_shared<FullyConnected>(1);
+  nn.add(fc3);
+
+  //nn.add(std::make_shared<Logistic>());
+
   auto loss = std::make_shared<SquaredLoss>();
   nn.add(loss);
-  while (true){
+
+
+  while (true) {
     for (int i = 0; i < 10000; i++) {
-      nn.train(16);
+      nn.train(provider, 16);
     }
-    nn.predict(10000);
-    std::cout << af::sum<float>(loss->output_ - nn.currentbatch_.targets) / 10000.f << std::endl;
-    std::cout << nn.currentbatch_.targets.dims(3) << std::endl;
+    nn.test(provider, 10000);
+    auto realtargets = provider.current_batch_.targets;
+    auto predtargets = loss->output_;
+    std::cout << af::sum<float>(realtargets - predtargets) / 10000.f << std::endl;
   }
 
   return 0;
