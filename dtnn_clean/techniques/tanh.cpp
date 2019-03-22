be51139677ba50037e5ce360dbfe3199c3f0a383
Seppo Kilpelainen <seppo.kilpelainen@gmail.com>
2019-03-22 13:34:37 +0200

    tanh activation function implementation

---
+++ dtnn_clean/techniques/tanh.cpp
@@ -0,0 +1,13 @@
+#include "tanh.hpp"
+
+namespace dtnn {
+  void Tanh::forward(Feed &f) {
+    activation_ = af::tanh(f.signal);
+    f.signal = activation_;
+  }
+  void Tanh::backward(Feed &f) {
+    f.signal = f.signal * (1.f - af::pow(activation_, 2));
+  }
+  template <class Archive> void Tanh::serialize(Archive &ar) {
+  }
+}
