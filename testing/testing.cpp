43899a0f44ea498ee771cd281fe404368cc4432e
Seppo Kilpelainen <seppo.kilpelainen@gmail.com>
2019-04-02 14:26:09 +0300

    test multiple backends

--- testing/testing.cpp
+++ testing/testing.cpp
@@ -1,2 +1,37 @@
-#define CATCH_CONFIG_MAIN
-#include <catch.hpp>
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+#include <arrayfire.h>
+
+int main(int argc, char* argv[]) {
+  std::cout << "available backends: " << af::getBackendCount() << std::endl;
+
+  int backends = af::getAvailableBackends();
+  bool cpu = backends & AF_BACKEND_CPU;
+  bool cuda = backends & AF_BACKEND_CUDA;
+  bool opencl = backends & AF_BACKEND_OPENCL;
+
+  Catch::Session session;
+
+  if (cpu) {
+    std::cout << "testing cpu backend" << std::endl;
+    af::setBackend(AF_BACKEND_CPU);
+    af::info();
+    session.run(argc, argv);
+  }
+
+  if (opencl) {
+    std::cout << "testing opencl backend" << std::endl;
+    af::setBackend(AF_BACKEND_OPENCL);
+    af::info();
+    session.run(argc, argv);
+  }
+
+  if (cuda) {
+    std::cout << "testing cuda backend" << std::endl;
+    af::setBackend(AF_BACKEND_CUDA);
+    af::info();
+    session.run(argc, argv);
+  }
+
+  return 0;
+}
