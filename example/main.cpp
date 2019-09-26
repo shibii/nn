31d2dde29f1255d80dc67d8b2980bc4247e73dbd
Seppo Kilpelainen <seppo.kilpelainen@gmail.com>
2019-09-26 19:13:17 +0300

    updated example program

--- example/main.cpp
+++ example/main.cpp
@@ -1,54 +1,130 @@
 #include <iostream>
 #include <ctime>
+#include <chrono>
 #include <dtnn.hpp>
+
+#include "arrayfire_util.hpp"
+#include "mnistSampler.hpp"
 
 int main(unsigned int argc, const char* argv[]) {
   using namespace dtnn;
 
+  //af::setBackend(AF_BACKEND_CPU);
+  //af::setBackend(AF_BACKEND_CUDA);
+  af::setBackend(AF_BACKEND_OPENCL);
+  af::info();
+
   af::setSeed(time(NULL));
 
-  af::array training_inputs = af::randu(af::dim4(1, 1, 1, 10000)) * af::Pi;
-  af::array training_targets = af::sin(training_inputs);
+  af::array training_images, training_targets;
+  MNIST::parseMnist("mnist/train-images.idx3-ubyte", "mnist/train-labels.idx1-ubyte", training_images, training_targets);
+  auto training_provider = TrainingBatchProvider(training_images, training_targets);
+  std::cout << "training samples: " << training_provider.sample_count() << std::endl;
 
-  af::array test_inputs = af::randu(af::dim4(1, 1, 1, 10000)) * af::Pi;
-  af::array test_targets = af::sin(test_inputs);
+  af::array test_images, test_targets;
+  MNIST::parseMnist("mnist/t10k-images.idx3-ubyte", "mnist/t10k-labels.idx1-ubyte", test_images, test_targets);
+  auto test_provider = TrainingBatchProvider(test_images, test_targets);
+  std::cout << "test samples: " << test_provider.sample_count() << std::endl;
 
-  auto training_provider = TrainingBlob(training_inputs.host<float>(), training_targets.host<float>(),
-    training_inputs.dims(), training_targets.dims());
+  auto optimizer = std::make_shared<Momentum>();
+  Network nn(training_provider.sample_dimensions(), optimizer);
 
-  auto test_provider = TrainingBlob(test_inputs.host<float>(), test_targets.host<float>(),
-    test_inputs.dims(), test_targets.dims());
+  auto conv1 = std::make_shared<Convolutional>(3, 3, 1, 1, 0, 0, 10);
+  nn.add(conv1);
+  nn.add(std::make_shared<MaxPool>(2, 2, 2, 2, 0, 0));
+  nn.add(std::make_shared<ReLU>());
 
-  auto optimizer = std::make_shared<SGD>(1e-3f);
+  auto conv2 = std::make_shared<Convolutional>(3, 3, 1, 1, 0, 0, 20);
+  nn.add(conv2);
+  nn.add(std::make_shared<MaxPool>(2, 2, 2, 2, 0, 0));
+  nn.add(std::make_shared<ReLU>());
 
-  Network nn(training_provider.input_dimensions(), optimizer);
-
-  auto fc1 = std::make_shared<FullyConnected>(20);
+  auto fc1 = std::make_shared<FullyConnected>(40);
   nn.add(fc1);
+  nn.add(std::make_shared<ReLU>());
 
-  nn.add(std::make_shared<Logistic>());
+  auto out = std::make_shared<FullyConnected>(10);
+  nn.add(out);
 
-  auto fc2 = std::make_shared<FullyConnected>(20);
-  nn.add(fc2);
+  auto classifier = std::make_shared<SoftmaxCrossEntropy>();
+  nn.add(classifier);
 
-  nn.add(std::make_shared<Logistic>());
+  dim_t batch_size = 16;
 
-  auto fc3 = std::make_shared<FullyConnected>(1);
-  nn.add(fc3);
+  std::vector<float> indices(training_provider.sample_count());
+  std::iota(indices.begin(), indices.end(), 0);
 
-  //nn.add(std::make_shared<Logistic>());
+  while (true) {
+    auto start = std::chrono::high_resolution_clock::now();
+    std::cout << std::endl << "training";
 
-  auto loss = std::make_shared<SquaredLoss>();
-  nn.add(loss);
+    std::shuffle(indices.begin(), indices.end(), std::mt19937{ std::random_device{}() });
 
-  for (int epochs = 0; epochs < 25; epochs++) {
-    for (int i = 0; i < 1000; i++) {
-      auto training_batch = training_provider.batch(16);
-      nn.train(training_batch);
+    for (int i = 0; i < training_provider.sample_count(); i += batch_size) {
+
+      std::vector<float> batch_indices(indices.begin() + i, indices.begin() + i + batch_size);
+      auto batch = training_provider.batch(batch_indices);
+
+      //auto batch = training_provider.batch(i, batch_size);
+
+      //try {
+        Hyperparameters hp;
+        hp.learningrate = 1e-3f;
+        hp.weight_decay = 1e-6f;
+        nn.train(batch, hp);
+      //}
+      //catch (af::exception& e) {
+      //  fprintf(stderr, "%s\n", e.what());
+      //  throw;
+      //}
     }
-    auto test_batch = test_provider.batch(10000);
-    auto result = nn.test(test_batch);
-    std::cout << "network loss: " << result.loss() << " rmse: " << result.rmse() << std::endl;
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
+    std::cout << std::endl << "execution time: " << duration.count() << " seconds";
+
+    float loss = 0.f;
+    float accuracy = 0.f;
+    float tp = 0.f;
+    float tn = 0.f;
+    float fp = 0.f;
+    float fn = 0.f;
+
+    start = std::chrono::high_resolution_clock::now();
+    std::cout << std::endl << "testing";
+    int batches = 0;
+    for (int i = 0; i < test_provider.sample_count(); i += batch_size) {
+
+      auto batch = test_provider.batch(i, batch_size);
+      auto result = nn.test(batch);
+      batches++;
+      loss += result.loss();
+      accuracy += result.accuracy();
+
+      //tp += result.true_positive(.5f);
+      //tn += result.true_negative(.5f);
+      //fp += result.false_positive(.5f);
+      //fn += result.false_negative(.5f);
+    }
+    //float precision = tp / (tp + fp);
+    //float recall = tp / (tp + fn);
+    //float f1 = (2 * precision * recall) / (precision + recall);
+
+    std::cout << std::endl << "loss: " << loss / (float)batches
+      << " accuracy: " << accuracy / (float)batches;
+      //<< " tp: " << tp
+      //<< " tn: " << tn
+      //<< " fp: " << fp
+      //<< " fn: " << fn
+      //<< " precision: " << precision
+      //<< " recall: " << recall
+      //<< " f1: " << f1;
+    stop = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
+    std::cout << std::endl << "execution time: " << duration.count() << " seconds";
+
+    //std::ofstream ostream("network.json", std::ios::binary);
+    //Serializer::serializeJSON(ostream, nn);
   }
 
   return 0;
