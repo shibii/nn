8c4d34cc85d68ec87d2a9dc14dc1ccf39f6da92e
Seppo Kilpelainen <seppo.kilpelainen@gmail.com>
2019-11-14 18:10:22 +0200

    removed spurious abstraction

--- dtnn_clean/serializer.hpp
+++ dtnn_clean/serializer.hpp
@@ -11,48 +11,33 @@ namespace nn {
   public:
     template <typename... ObjectTypes>
     static void serializeJSON(std::ostream &stream, ObjectTypes &... objects) {
-      serialize<cereal::JSONOutputArchive>(stream, objects...);
+      cereal::JSONOutputArchive oarchive(stream);
+      oarchive(objects...);
     }
     template <typename... ObjectTypes>
     static void deserializeJSON(std::istream &stream, ObjectTypes &... objects) {
-      deserialize<cereal::JSONInputArchive>(stream, objects...);
+      cereal::JSONInputArchive iarchive(stream);
+      iarchive(objects...);
     }
     template <typename... ObjectTypes>
     static void serializeBinary(std::ostream &stream, ObjectTypes &... objects) {
-      serialize<cereal::PortableBinaryOutputArchive>(stream, objects...);
+      cereal::PortableBinaryOutputArchive oarchive(stream);
+      oarchive(objects...);
     }
     template <typename... ObjectTypes>
     static void deserializeBinary(std::istream &stream, ObjectTypes &... objects) {
-      deserialize<cereal::PortableBinaryInputArchive>(stream, objects...);
+      cereal::PortableBinaryInputArchive iarchive(stream);
+      iarchive(objects...);
     }
     template <typename... ObjectTypes>
     static void serializeXML(std::ostream &stream, ObjectTypes &... objects) {
-      serialize<cereal::XMLOutputArchive>(stream, objects...);
+      cereal::XMLOutputArchive oarchive(stream);
+      oarchive(objects...);
     }
     template <typename... ObjectTypes>
     static void deserializeXML(std::istream &stream, ObjectTypes &... objects) {
-      deserialize<cereal::XMLInputArchive>(stream, objects...);
-    }
-  private:
-    template <typename OutputArchiveType, typename... ObjectTypes>
-    static void serialize(std::ostream &stream, ObjectTypes &... objects) {
-      OutputArchiveType oarchive(stream);
-      oarchive(objects...);
-    }
-    template <typename InputArchiveType, typename... ObjectTypes>
-    static void deserialize(std::istream &stream, ObjectTypes &... objects) {
-      InputArchiveType iarchive(stream);
+      cereal::XMLInputArchive iarchive(stream);
       iarchive(objects...);
-    }
-    template <typename OutputArchiveType, typename... ObjectTypes>
-    static void serialize(const char *path, ObjectTypes &... objects) {
-      std::ofstream stream(path, std::ios::binary);
-      serialize<OutputArchiveType>(stream, objects...);
-    }
-    template <typename InputArchiveType, typename... ObjectTypes>
-    static void deserialize(const char *path, ObjectTypes &... objects) {
-      std::ifstream stream(path, std::ios::binary);
-      deserialize<InputArchiveType>(stream, objects...);
     }
   };
 }
