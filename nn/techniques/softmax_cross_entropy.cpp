4502c4c65c09c9c40826b533e1a59512bf76ce98
Seppo Kilpelainen <seppo.kilpelainen@gmail.com>
2019-12-08 01:20:01 +0200

    tweaked the softmax cross entropy loss

--- nn/techniques/softmax_cross_entropy.cpp
+++ nn/techniques/softmax_cross_entropy.cpp
@@ -8,10 +8,10 @@ af::array SoftmaxCrossEntropy::error(Feed &f, af::array target) const {
 af::array SoftmaxCrossEntropy::loss(Feed &f, af::array target) const {
   // shifted values are used to calculate the log of softmax
   af::array max = af::max(f.signal);
-  af::array shifted_input = af::batchFunc(f.signal, max, util::sub);
-  af::array expsum = af::sum(af::exp(shifted_input));
-  af::array logsoftmax = af::batchFunc(max, af::log(expsum), util::add);
-  logsoftmax = af::batchFunc(f.signal, logsoftmax, util::sub);
+  af::array expsum = af::sum(af::exp(af::batchFunc(f.signal, max, util::sub)));
+  af::array logexpsum = af::log(expsum);
+  af::array logsoftmax = af::batchFunc(f.signal, max, util::sub);
+  logsoftmax = af::batchFunc(logsoftmax, logexpsum, util::sub);
   return -(target * logsoftmax);
 }
 af::array SoftmaxCrossEntropy::output(Feed &f) const {
